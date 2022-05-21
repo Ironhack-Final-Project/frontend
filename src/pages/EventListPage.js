21d30253cc83194376874558ba312b7687ff95e6
Valentin Paul <valentin@vamptech.de>
2022-05-21 11:00:10 +0200

    add functionality for users to attend an event

--- src/pages/EventListPage.js
+++ src/pages/EventListPage.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import "./EventPageList.css";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 
 function EventListPage() {
+  const [userId, setUserId] = useState(null);
+
+  const { user } = useContext(AuthContext);
   const [events, setEvents] = useState(null);
 
   useEffect(() => {
+    setUserId(user);
+    updateEvents();
+  }, [user]);
+
+  const updateEvents = () => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/events`)
       .then((response) => {
@@ -15,7 +25,22 @@ function EventListPage() {
       .catch((e) => {
         console.log("error getting events list...", e);
       });
-  }, []);
+  };
+
+  const attendEvent = (eventId) => {
+    // e.preventDefault();
+
+    console.log(userId);
+    axios
+      .put(`${process.env.REACT_APP_API_URL}/events/${eventId}`, {
+        id: userId._id,
+      })
+      .then((response) => {
+        console.log(response.data);
+        updateEvents();
+      })
+      .catch((err) => console.log("error attending event...", err));
+  };
 
   return (
     <div className="events-list">
@@ -30,7 +55,25 @@ function EventListPage() {
               <p>{element.date}</p>
               <p>Description: {element.description}</p>
               <p>Price: {element.cost}</p>
-              <p>{element.attendees}</p>
+
+              <button
+                onClick={() => {
+                  attendEvent(element._id);
+                }}
+              >
+                Attend
+              </button>
+
+              <div className="attendees">
+                <h4>Attending:</h4>
+                {element.attendees.map((element) => {
+                  return (
+                    <>
+                      <p>{element.username}</p>
+                    </>
+                  );
+                })}
+              </div>
             </div>
           );
         })
