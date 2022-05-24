6d2906a7fc9195b73623e6ec7e2a88df09b6e8af
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-05-24 12:49:59 +0200

    change to link from env file for deployment

--- src/components/utitlityFunctions.js
+++ src/components/utitlityFunctions.js
@@ -11,7 +11,7 @@ const uploadImage = (file) => {
 
 const api = axios.create({
     // make sure you use PORT = 5005 (the port where our server is running)
-    baseURL: "http://localhost:5005/api"
+    baseURL: process.env.REACT_APP_API_URL
     // withCredentials: true // => you might need this option if using cookies and sessions
 });
 
