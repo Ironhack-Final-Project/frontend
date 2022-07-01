5cc00429dc864ebe69daa63f2467143c820af6b1
Valentin Paul <valentin@vamptech.de>
2022-07-01 15:05:35 +0200

    changed about us  with map

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,26 @@
+import mapboxgl from "mapbox-gl";
+
 function AboutUs() {
+
+   <link href='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
+   
+  mapboxgl.accessToken = 'pk.eyJ1IjoidmFsZXdhbGQiLCJhIjoiY2w1MmdoYnBpMGZkYTNtcWl4cjl5bWo3MiJ9.dlNqXwgqNGwCR35-1qBgyA';
+
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+    projection: 'globe' // display the map as a 3D globe
+});
+
+map.on('style.load', () => {
+  map.setFog({}); 
+});
+
   return (
     <div className="about-us">
+   
       <h1>About Us</h1>
       <p style={{ width: 40 + "%", margin: "auto" }}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a mauris
@@ -19,6 +39,13 @@ function AboutUs() {
         dapibus odio, id eleifend sem congue in. Proin luctus in est ut mattis.
         Phasellus id lobortis quam, non fermentum dui.
       </p>
+
+
+
+
+
+
+
     </div>
   );
 }
